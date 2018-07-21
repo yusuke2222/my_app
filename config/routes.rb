b53dcc57198bbd3383f7c69f178ed88c37182ea8
kamikura <>
2018-07-21 17:36:50 +0900

    first_commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'posts' => 'posts#index'
+
+  get '/' => 'home#top'
+
+  get 'about' => 'home#about'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
