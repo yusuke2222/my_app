8d48db582ffbe52e43339ffd87300f86ed7fcd94
kamikura <>
2018-07-23 13:49:06 +0900

    create new

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'posts' => 'posts#index'
+  get 'posts' => 'posts#new'
+  get 'posts' => 'posts#show'
 
   get '/' => 'home#top'
-
   get 'about' => 'home#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
